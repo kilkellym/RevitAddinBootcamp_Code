9e209485eaded30cbd59e3da4dfce087b5faf49a
kilkellym <michael@archsmarter.com>
2024-11-18 16:20:45 -0500

    complete

--- RevitAddinBootcamp/cmdChallenge01.cs
+++ RevitAddinBootcamp/cmdChallenge01.cs
@@ -10,12 +10,15 @@
             UIDocument uidoc = uiapp.ActiveUIDocument;
             Document doc = uidoc.Document;
 
-            // FIZZBUZZ Challenge afadf
+            // FIZZBUZZ Challenge 
 
             // 1. set variables
             int numFloors = 250;
             double currentElev = 0;
             int floorHeight = 15;
+            int fizzCount = 0;
+            int buzzCount = 0;
+            int fizzbuzzCount = 0;
 
             // 2. get titleblock
             FilteredElementCollector tbCollector = new FilteredElementCollector(doc);
@@ -43,16 +46,32 @@
                 }
             }
 
+            // 4b. check for null values
+            if(fpVFT == null)
+            {   
+                TaskDialog.Show("Error", "There are no floor plan view family types in the model.");
+                return Result.Failed;
+            }
+            else if(cpVFT == null)
+            {
+                TaskDialog.Show("Error", "There are no ceiling plan view family types in the model.");
+                return Result.Failed;
+            }
+
             // 5. create transaction
             Transaction t = new Transaction(doc);
             t.Start("FIZZ BUZZ Challenge");
 
             // 6. create floors and check FIZZBUZZ
-            for (int i = 1; i <= numFloors; i++)
+            for (int i = 0; i <= numFloors; i++)
             {
                 // 7. create level
                 Level newLevel = Level.Create(doc, currentElev);
-                newLevel.Name = "LEVEL " + i.ToString();
+
+                if(i < 10)
+                    newLevel.Name = $"LEVEL 0{i}";
+                else
+                    newLevel.Name = $"LEVEL {i}";
 
                 // 8. check for FIZZ, BUZZ, and FIZZBUZZ
                 if (i % 3 == 0 && i % 5 == 0)
@@ -60,25 +79,36 @@
                     // 8a. FIZZBUZZ - create sheet
                     ViewSheet newSheet = ViewSheet.Create(doc, tblockId);
                     newSheet.SheetNumber = i.ToString();
-                    newSheet.Name = "FIZZBUZZ_" + i.ToString();
+                    //newSheet.Name = "FIZZBUZZ_" + i.ToString();
+                    newSheet.Name = $"FIZZBUZZ_{i}";
 
                     // BONUS
                     ViewPlan bonusPlan = ViewPlan.Create(doc, fpVFT.Id, newLevel.Id);
+                    bonusPlan.Name = $"FIZZBUZZ_{i}";
                     Viewport newVP = Viewport.Create(doc, newSheet.Id, bonusPlan.Id, new XYZ(1, .5, 0));
+
+                    fizzbuzzCount++;
                 }
                 else if (i % 3 == 0)
                 {
                     // 8b. FIZZ - create floor plan
                     ViewPlan newFloorPlan = ViewPlan.Create(doc, fpVFT.Id, newLevel.Id);
+                    newFloorPlan.Name = $"FIZZ_{i}";
+
+                    fizzCount ++;
                 }
                 else if (i % 5 == 0)
                 {
                     // 8c. BUZZ - create ceiling plan
                     ViewPlan newCeilingPlan = ViewPlan.Create(doc, cpVFT.Id, newLevel.Id);
+                    newCeilingPlan.Name = $"BUZZ_{i}";
+
+                    buzzCount ++;
                 }
 
                 // 9. increment elevation
                 currentElev += floorHeight;
+                //currentElev = currentElev + floorHeight;
             }
 
             // 10. commit and close transaction
@@ -86,11 +116,14 @@
             t.Dispose();
 
             // 11. alert user
-            TaskDialog.Show("Complete", "Created " + numFloors + " levels.");
-            //TaskDialog.Show("Complete", $"Created {numFloors} levels.");
+            TaskDialog.Show("Complete", $"Created {numFloors} levels." +
+                $"Created {fizzCount} FIZZ views. " +
+                $"Create {buzzCount} BUZZ views. " +
+                $"Created {fizzbuzzCount} FIZZBUZZ sheets.");
 
             return Result.Succeeded;
         }
+
         internal static PushButtonData GetButtonData()
         {
             // use this method to define the properties for this command in the Revit ribbon
